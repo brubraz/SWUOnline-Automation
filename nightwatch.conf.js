4201212e997e03ae16a063c6b54cc2504607d29f
Bruno Braz <bbrazsilveira@gmail.com>
2025-01-28 15:45:31 -0300

    chore: added headless configuration

--- nightwatch.conf.js
+++ nightwatch.conf.js
@@ -1,34 +1,6 @@
-// Refer to the online docs for more details:
-// https://nightwatchjs.org/gettingstarted/configuration/
-//
-
-//  _   _  _         _      _                     _          _
-// | \ | |(_)       | |    | |                   | |        | |
-// |  \| | _   __ _ | |__  | |_ __      __  __ _ | |_   ___ | |__
-// | . ` || | / _` || '_ \ | __|\ \ /\ / / / _` || __| / __|| '_ \
-// | |\  || || (_| || | | || |_  \ V  V / | (_| || |_ | (__ | | | |
-// \_| \_/|_| \__, ||_| |_| \__|  \_/\_/   \__,_| \__| \___||_| |_|
-//             __/ |
-//            |___/
-
 module.exports = {
-  // An array of folders (excluding subfolders) where your tests are located;
-  // if this is not specified, the test source must be passed as the second argument to the test runner.
   src_folders: ['test','nightwatch'],
 
-  // See https://nightwatchjs.org/guide/concepts/page-object-model.html
-  page_objects_path: [],
-
-  // See https://nightwatchjs.org/guide/extending-nightwatch/adding-custom-commands.html
-  custom_commands_path: [],
-
-  // See https://nightwatchjs.org/guide/extending-nightwatch/adding-custom-assertions.html
-  custom_assertions_path: [],
-
-  // See https://nightwatchjs.org/guide/extending-nightwatch/adding-plugins.html
-  plugins: [],
-
-  // See https://nightwatchjs.org/guide/concepts/test-globals.html
   globals_path: 'globals.ts',
 
   webdriver: {},
@@ -49,39 +21,36 @@ module.exports = {
       },
 
       desiredCapabilities: {
-        browserName: 'chrome'
-      },
-
-      webdriver: {
-        start_process: true,
-        server_path: ''
-      },
-
-    },
-
-    chrome: {
-      desiredCapabilities: {
         browserName: 'chrome',
         'goog:chromeOptions': {
-          // More info on Chromedriver: https://sites.google.com/a/chromium.org/chromedriver/
           args: [
-            //'--no-sandbox',
-            //'--ignore-certificate-errors',
-            //'--allow-insecure-localhost',
-            //'--headless=new'
+            '--no-sandbox',
+            '--disable-dev-shm-usage',
           ]
         }
       },
 
       webdriver: {
         start_process: true,
-        server_path: '',
-        cli_args: [
-          // --verbose
-        ]
-      }
+        server_path: ''
+      },
     },
 
+    headless: {
+      extends: 'default',
+      desiredCapabilities: {
+        browserName: 'chrome',
+        'goog:chromeOptions': {
+          args: [
+            '--headless',
+            '--no-sandbox',
+            '--disable-dev-shm-usage',
+            '--disable-gpu',
+            '--window-size=1920,1080'
+          ]
+        }
+      }
+    },
   },
 
   usage_analytics: {
